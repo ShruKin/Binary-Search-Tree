185a39008f5d877a11a3e4a75b43fe1942f8c15e
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2019-11-24 01:47:40 +0530

    Added a lot more than you were expecting + the developer out there is disgusted enough to even write a sensible commit mesage!

---
+++ bstree.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct BSTree{
+    int data;
+    struct BSTree *left, *right;
+};
+
+struct BSTree* insert(struct BSTree *root, int data){
+    struct BSTree *node = (struct BSTree *)malloc(sizeof(struct BSTree));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    struct BSTree *ptr=root, *prev_ptr=NULL;
+
+    if(root == NULL)
+        root = node;
+
+    else{
+        while(ptr != NULL){
+            prev_ptr = ptr;
+            if(data < ptr->data)
+                ptr = ptr->left;
+            else
+                ptr = ptr->right;
+        }
+
+        if(data < prev_ptr->data)
+            prev_ptr->left = node;
+        else
+            prev_ptr->right = node;
+    }
+
+    return root;
+}
+
+struct BSTree* insert_recur(struct BSTree *node, int data) 
+{ 
+    /* If the tree is empty, return a new node */
+    if (node == NULL){
+        node = (struct BSTree *)malloc(sizeof(struct BSTree));
+        node->data = data;
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+    } 
+  
+    /* Otherwise, recur down the tree */
+    if (data < node->data) 
+        node->left  = insert(node->left, data); 
+    else if (data > node->data) 
+        node->right = insert(node->right, data);    
+  
+    /* return the (unchanged) node pointer */
+    return node; 
+}
+
+struct BSTree* create(){
+    struct BSTree *root = NULL;
+    int data;
+
+    printf("Enter data: ");
+    scanf("%d", &data);
+
+    while(data != -1){
+        root = insert(root, data);        
+        printf("Enter data: ");
+        scanf("%d", &data);
+    }
+
+    return root;
+}
+
+struct BSTree* create_recur(){
+    struct BSTree *root = NULL;
+    int data;
+
+    printf("Enter data: ");
+    scanf("%d", &data);
+    if( data != -1)
+        root = insert_recur(root, data);        
+
+    while(data != -1){
+        printf("Enter data: ");
+        scanf("%d", &data);
+
+        if(data != -1)
+            insert_recur(root, data);        
+    }
+
+    return root;
+}
+
+void preorder(struct BSTree *root)
+{
+	if(root != NULL)
+	{
+		printf("%d ",root->data);	
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+void postorder(struct BSTree *root)
+{
+	if(root != NULL)
+	{
+		preorder(root->left);
+		preorder(root->right);
+		printf("%d ",root->data);	
+	}
+}
+
+void inorder(struct BSTree *root)
+{
+	if(root != NULL)
+	{
+		preorder(root->left);
+		printf("%d ",root->data);	
+		preorder(root->right);
+	}
+}
+
+int height(struct BSTree *root)  
+{  
+    if (root == NULL)  
+        return 0;  
+    else
+    {  
+        int lheight = height(root->left);  
+        int rheight = height(root->right);  
+
+        if (lheight > rheight)  
+            return(lheight + 1);  
+        else 
+            return(rheight + 1);  
+    }  
+}  
+
+int search(struct BSTree *root, int data){
+    if(root == NULL)
+        return 0;
+
+    if(root->data == data)
+        return 1;
+
+    if(data < root->data)
+        return search(root->left, data);
+    else
+        return search(root->right, data);
+}
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include "visualize.cpp"
+
+int main(int argc, char const *argv[])
+{
+    int ch, data;
+    struct BSTree *root;
+
+    while(1){
+        printf("MENU");
+        printf("\n1. Create");
+        printf("\n2. Insert");
+        printf("\n3. Visualize");
+        printf("\n4. Inorder");
+        printf("\n5. Preorder");
+        printf("\n6. Postorder");
+        printf("\n7. Search");
+        printf("\n0. EXIT");
+
+        printf("\nEnter choice: ");
+        scanf("%d", &ch);
+
+        printf("\n");
+        switch(ch){
+            case 0: exit(0);    break;
+
+            case 1: root = create();    break;
+
+            case 2:
+                printf("Enter data: ");
+                scanf("%d", &data);
+                root = insert(root, data);
+                break;
+
+            case 3: visualize(root);    break;
+
+            case 4: inorder(root);  break;
+
+            case 5: preorder(root); break;
+
+            case 6: postorder(root);    break;
+
+            case 7: 
+                printf("Enter data: ");
+                scanf("%d", &data);
+
+                if(search(root, data))
+                    printf("%d found in the BSTree!", data);
+                else
+                    printf("%d not found in the BSTree!", data);
+
+        }
+        printf("\n\n");
+    }
+
+    return 0;
+}
